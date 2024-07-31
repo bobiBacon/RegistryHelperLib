10f0e65e1f9bc872eeeafe527b8f17c1496225aa
Handsome Steve <51927714+handsome-steve@users.noreply.github.com>
2024-08-01 06:42:33 +0930

    Minor modifications to follow Java Docs best practices (#2)
    * Minor modifications to follow Java Docs best practices.
    Refactored 'registry' to 'REGISTRY'.
    Replaced 'IF Statements' with inline 'Objects.requireNonNull'.

--- src/main/java/net/bobbacon/api/RegistryHelper.java
+++ src/main/java/net/bobbacon/api/RegistryHelper.java
@@ -4,23 +4,29 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
+
 public class RegistryHelper<T> {
-    private final Registry<T> registry;
+    private final Registry<T> REGISTRY;         // Refactored to upper case as per Java Docs best practices.
     private final String MOD_ID;
 
     public RegistryHelper(@NotNull Registry<T> registry,@NotNull String modId) {
-        if (registry==null){
-            throw new NullPointerException("registry is null");
-        }
-        if (modId==null){
-            throw new NullPointerException("The Mod Id is Null");
-        }
-        this.registry=registry;
-        MOD_ID = modId;
+        // Replaced 'IF Statements' with inline 'Objects.requireNonNull' as per Java Docs best practices.
+        /*
+        * NOTE: 'Objects.requireNonNull' is a helpful method to spot NullPointerExceptions effectively while developing.
+        *       But in fact it's throwing a RuntimeException which could crash the application (the worst thing ever in production).
+        *       However, in this context it is imperative to catch this error before allowing it to continue further on since we are accessing registries.
+        *
+        * NOTE: This program will build when initializing this class;
+        *       'RegistryHelper<T> registryHelper=new RegistryHelper<T>(null,null);',
+        *       but will only throw the 'NullPointerException' at runtime.
+        * */
+        this.REGISTRY = Objects.requireNonNull(registry, "[ERROR] Oops! Something has gone horribly wrong: 'registry' is 'null' in the parsed parameter");
+        MOD_ID = Objects.requireNonNull(modId, "[ERROR] Oops! Something has gone horribly wrong: 'modId' is 'null' in the parsed parameter");
     }
 
 
     public T register(String name,T element){
-        return Registry.register(registry, Identifier.of(MOD_ID,name),element);
+        return Registry.register(REGISTRY, Identifier.of(MOD_ID,name),element);
     }
 }
